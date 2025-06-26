2366c7c507fd20e759ff5cf526444478c57609a9
asormar <sorolladev@gmail.com>
2025-06-27 01:13:02 +0200

    cors

--- DizzWord/rxconfig.py
+++ DizzWord/rxconfig.py
@@ -3,6 +3,10 @@ import reflex as rx
 config = rx.Config(
     app_name="DizzWord",
     api_url="https://dizzword.up.railway.app",
+    cors_allowed_origins=[
+        "https://dizz-word.vercel.app",
+        "http://localhost:3000/"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
