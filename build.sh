de3d500d39218b24dbab68757d65f8f5c5567039
asormar <sorolladev@gmail.com>
2025-06-26 22:06:48 +0200

    build added

---
+++ build.sh
@@ -0,0 +1,9 @@
+source ./venv/Scripts/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+reflex init
+reflex export --frontend-only
+rm -rf public
+powershell -command "Expand-Archive -Path 'frontend.zip' -DestinationPath 'public'"
+rm frontend.zip
+deactivate
