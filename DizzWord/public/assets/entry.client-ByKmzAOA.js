6cd859559c6d4b759fee2581f17583080d421a35
asormar <sorolladev@gmail.com>
2025-06-27 01:08:02 +0000

    Update static build

--- DizzWord/public/assets/entry.client-ByKmzAOA.js
+++ DizzWord/public/assets/entry.client-ByKmzAOA.js
@@ -1,4 +1,4 @@
-import{j as Kv}from"./jsx-runtime-D_zvdyIk.js";import{r as gy,a as _l,i as Wv,u as by,F as zy,R as Ty,b as Ay,d as My,c as Ey,g as Dy,e as Oy,f as Uy,h as _y,j as Ry,k as Hy,l as Ny,m as qy,n as By}from"./chunk-NL6KNZEE-CWTkl3TD.js";import{r as Yy,a as ry}from"./index-BKwYYFjP.js";var Wc={exports:{}},ie={},$c={exports:{}},kc={};/**
+import{j as Kv}from"./jsx-runtime-D_zvdyIk.js";import{r as gy,a as _l,i as Wv,u as by,F as zy,R as Ty,b as Ay,d as My,c as Ey,g as Dy,e as Oy,f as Uy,h as _y,j as Ry,k as Hy,l as Ny,m as qy,n as By}from"./chunk-NL6KNZEE-CCkT4AiD.js";import{r as Yy,a as ry}from"./index-BlaaLJt4.js";var Wc={exports:{}},ie={},$c={exports:{}},kc={};/**
  * @license React
  * scheduler.production.js
  *
